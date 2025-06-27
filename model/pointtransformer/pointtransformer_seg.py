02afaf955a247c0b0ac52f7341ce72fbf6ddb0b8
zhoujinhai <932853432@qq.com>
2025-06-27 17:20:51 +0800

    Update pointtransformer_seg.py

--- model/pointtransformer/pointtransformer_seg.py
+++ model/pointtransformer/pointtransformer_seg.py
@@ -67,6 +67,41 @@ class TransitionDown(nn.Module):
         return [p, x, o]
 
 
+class TransitionDownExport(nn.Module):
+    def __init__(self, in_planes, out_planes, stride=1, nsample=16):
+        super().__init__()
+        self.stride, self.nsample = stride, nsample
+        if stride != 1:
+            self.linear = nn.Linear(3+in_planes, out_planes, bias=False)
+            self.pool = nn.MaxPool1d(nsample)
+        else:
+            self.linear = nn.Linear(in_planes, out_planes, bias=False)
+        self.bn = nn.BatchNorm1d(out_planes)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, pxo):
+        p, x, o = pxo  # (n, 3), (n, c), (b)
+        if self.stride != 1:
+            # n_o, count = [o[0].item() // self.stride], o[0].item() // self.stride
+            # for i in range(1, o.shape[0]):
+            #     count += (o[i].item() - o[i-1].item()) // self.stride
+            #     n_o.append(count) 
+            # n_o = [o[0].item() // self.stride]
+            # n_o = torch.cuda.IntTensor(n_o)
+            # print("n_o  11111111: ", n_o)
+            n_o = (o[0:1] // self.stride)    # for export
+            # print("n_o  22222222: ", n_o) 
+            idx = pointops.furthestsampling(p, o, n_o)  # (m)
+            n_p = p[idx.long(), :]  # (m, 3)
+            x = pointops.queryandgroup(self.nsample, p, n_p, x, None, o, n_o, use_xyz=True)  # (m, 3+c, nsample) 
+            x = self.relu(self.bn(self.linear(x).transpose(1, 2).contiguous()))  # (m, c, nsample)
+            x = self.pool(x).squeeze(-1)  # (m, c)
+            p, o = n_p, n_o
+        else:
+            x = self.relu(self.bn(self.linear(x)))  # (n, c)
+        return [p, x, o]
+
+
 class TransitionUp(nn.Module):
     def __init__(self, in_planes, out_planes=None):
         super().__init__()
@@ -94,6 +129,40 @@ class TransitionUp(nn.Module):
         else:
             p1, x1, o1 = pxo1; p2, x2, o2 = pxo2
             x = self.linear1(x1) + pointops.interpolation(p2, p1, self.linear2(x2), o2, o1)
+        return x
+
+
+class TransitionUpExport(nn.Module):
+    def __init__(self, in_planes, out_planes=None):
+        super().__init__()
+        if out_planes is None:
+            self.linear1 = nn.Sequential(nn.Linear(2*in_planes, in_planes), nn.BatchNorm1d(in_planes), nn.ReLU(inplace=True))
+            self.linear2 = nn.Sequential(nn.Linear(in_planes, in_planes), nn.ReLU(inplace=True))
+        else:
+            self.linear1 = nn.Sequential(nn.Linear(out_planes, out_planes), nn.BatchNorm1d(out_planes), nn.ReLU(inplace=True))
+            self.linear2 = nn.Sequential(nn.Linear(in_planes, out_planes), nn.BatchNorm1d(out_planes), nn.ReLU(inplace=True))
+       
+
+    def forward(self, pxo1, pxo2=None):
+        if pxo2 is None:
+            _, x, o = pxo1  # (n, 3), (n, c), (b)  
+            s = torch.zeros(1, dtype=torch.int64, device=o.device)
+            e = o[0:1].to(torch.int64)
+            cnt = e[0] - s[0]      
+            # print(o, s, e, cnt)
+            x_b = x[s[0]:e[0]]
+            # print("xb: ", x_b)
+            x_b = torch.cat((x_b, self.linear2(x_b.sum(0, True) / cnt).repeat(cnt, 1)), 1)  
+            # print("xb2: ", x_b)
+            x = self.linear1(x_b) 
+            # print(x.shape)
+            return x
+        else:
+            p1, x1, o1 = pxo1; p2, x2, o2 = pxo2
+            x1 = self.linear1(x1)
+            x2 = self.linear2(x2) 
+            x_inter = pointops.interpolation_export(p2, p1, x2, o2, o1)
+            x = x1 + x_inter
         return x
 
 
@@ -173,6 +242,69 @@ class PointTransformerSeg(nn.Module):
         return x
 
 
+class PointTransformerSegExport(nn.Module):
+    def __init__(self, block, blocks, in_channels=6, n_cls=13):
+        super().__init__()
+        self.c = in_channels
+        self.in_planes, planes = in_channels, [32, 64, 128, 256, 512]
+        fpn_planes, fpnhead_planes, share_planes = 128, 64, 8
+        stride, nsample = [1, 4, 4, 4, 4], [8, 16, 16, 16, 16]
+        self.enc1 = self._make_enc(block, planes[0], blocks[0], share_planes, stride=stride[0], nsample=nsample[0])  # N/1
+        self.enc2 = self._make_enc(block, planes[1], blocks[1], share_planes, stride=stride[1], nsample=nsample[1])  # N/4
+        self.enc3 = self._make_enc(block, planes[2], blocks[2], share_planes, stride=stride[2], nsample=nsample[2])  # N/16
+        self.enc4 = self._make_enc(block, planes[3], blocks[3], share_planes, stride=stride[3], nsample=nsample[3])  # N/64
+        self.enc5 = self._make_enc(block, planes[4], blocks[4], share_planes, stride=stride[4], nsample=nsample[4])  # N/256
+        self.dec5 = self._make_dec(block, planes[4], 2, share_planes, nsample=nsample[4], is_head=True)  # transform p5
+        self.dec4 = self._make_dec(block, planes[3], 2, share_planes, nsample=nsample[3])  # fusion p5 and p4
+        self.dec3 = self._make_dec(block, planes[2], 2, share_planes, nsample=nsample[2])  # fusion p4 and p3
+        self.dec2 = self._make_dec(block, planes[1], 2, share_planes, nsample=nsample[1])  # fusion p3 and p2
+        self.dec1 = self._make_dec(block, planes[0], 2, share_planes, nsample=nsample[0])  # fusion p2 and p1
+        self.cls = nn.Sequential(nn.Linear(planes[0], planes[0]), nn.BatchNorm1d(planes[0]), nn.ReLU(inplace=True), nn.Linear(planes[0], n_cls))
+
+    def _make_enc(self, block, planes, blocks, share_planes=8, stride=1, nsample=16):
+        layers = []
+        layers.append(TransitionDownExport(self.in_planes, planes * block.expansion, stride, nsample))
+        self.in_planes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.in_planes, self.in_planes, share_planes, nsample=nsample))
+        return nn.Sequential(*layers)
+
+    def _make_dec(self, block, planes, blocks, share_planes=8, nsample=16, is_head=False):
+        layers = []
+        layers.append(TransitionUpExport(self.in_planes, None if is_head else planes * block.expansion))
+        self.in_planes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.in_planes, self.in_planes, share_planes, nsample=nsample))
+        return nn.Sequential(*layers)
+
+    def forward(self, feats):
+        p0 = feats[:, :3]
+        x0 = feats[:, 3:]
+        p0 = p0.contiguous()
+        x0 = x0.contiguous()
+        # o0 = torch.IntTensor([feats.shape[0]]).to(p0.device)  # (n, 3), (n, c), (b)
+        o0 = torch._shape_as_tensor(feats)[0].reshape(1).to(p0.device).to(torch.int32)   # for export onnx
+        # print("p0", p0.shape, x0.shape, o0, o0.shape)
+        x0 = p0 if self.c == 3 else torch.cat((p0, x0), 1)
+        p1, x1, o1 = self.enc1([p0, x0, o0]) 
+        p2, x2, o2 = self.enc2([p1, x1, o1])
+        p3, x3, o3 = self.enc3([p2, x2, o2])
+        p4, x4, o4 = self.enc4([p3, x3, o3])
+        p5, x5, o5 = self.enc5([p4, x4, o4])
+        # print("o5: ", o5, self.dec5[0]([p5, x5, o5])) 
+        x5 = self.dec5[1:]([p5, self.dec5[0]([p5, x5, o5]), o5])[1] 
+        x4 = self.dec4[0]([p4, x4, o4], [p5, x5, o5])
+        x4 = self.dec4[1:]([p4, self.dec4[0]([p4, x4, o4], [p5, x5, o5]), o4])[1]
+        x3 = self.dec3[1:]([p3, self.dec3[0]([p3, x3, o3], [p4, x4, o4]), o3])[1]
+        x2 = self.dec2[1:]([p2, self.dec2[0]([p2, x2, o2], [p3, x3, o3]), o2])[1]
+        x1 = self.dec1[1:]([p1, self.dec1[0]([p1, x1, o1], [p2, x2, o2]), o1])[1]
+        x = self.cls(x1)
+        return x
+
 def pointtransformer_seg_repro(**kwargs):
     model = PointTransformerSeg(PointTransformerBlock, [2, 3, 4, 6, 3], **kwargs)
+    return model
+
+def pointtransformer_seg_repro_export(**kwargs):
+    model = PointTransformerSegExport(PointTransformerBlock, [2, 3, 4, 6, 3], **kwargs)
     return model
