16b81b1631adf678ea2ff8596d54aa3378eaa5cd
zhoujinhai <932853432@qq.com>
2025-12-02 15:10:37 +0800

    Create Interval.h

---
+++ tool/inference_onnx_C++/AITools/Interval.h
@@ -0,0 +1,12 @@
+#ifndef KD_TREE_BASED_FARTHEST_POINT_SAMPLING_INTERVAL_H
+#define KD_TREE_BASED_FARTHEST_POINT_SAMPLING_INTERVAL_H
+
+
+class Interval {
+public:
+    float low, high;
+    Interval();
+};
+
+
+#endif //KD_TREE_BASED_FARTHEST_POINT_SAMPLING_INTERVAL_H
