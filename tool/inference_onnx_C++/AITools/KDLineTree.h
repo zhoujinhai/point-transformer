5fb9899b116644bf30e0924917aa3733c89c3bfd
zhoujinhai <932853432@qq.com>
2025-12-02 15:11:47 +0800

    add bucket fps Create KDLineTree.h

---
+++ tool/inference_onnx_C++/AITools/KDLineTree.h
@@ -0,0 +1,26 @@
+#ifndef FPS_CPU_KDLINETREE_H
+#define FPS_CPU_KDLINETREE_H
+
+#include "KDTreeBase.h"
+class KDLineTree :public KDTreeBase{
+public:
+
+    KDLineTree(KDTreeBase::Points &data, int pointSize, int treeHigh, KDTreeBase::Points &samplePoints);
+    ~KDLineTree();
+    std::vector<NodePtr> KDNode_list;
+
+    int high_;
+
+    Point max_point() override;
+
+    void update_distance(const Point &ref_point) override;
+
+    void sample(int sample_num) override;
+
+    bool leftNode(int high, int count) override;
+
+    void addNode(NodePtr p) override;
+};
+
+
+#endif //FPS_CPU_KDLINETREE_H
