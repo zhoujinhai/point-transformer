74d524580def41b4f2c9086fc56302ac0863f985
zhoujinhai <932853432@qq.com>
2025-12-16 15:27:49 +0800

    Update pointops.py

--- lib/pointops/functions/pointops.py
+++ lib/pointops/functions/pointops.py
@@ -99,6 +99,28 @@ class Grouping(Function):
 
 grouping = Grouping.apply
 
+def grouping2(idx,
+             feat,
+             xyz,
+             new_xyz=None,
+             with_xyz=False):
+    if new_xyz is None:
+        new_xyz = xyz
+    assert xyz.is_contiguous() and feat.is_contiguous()
+    m, nsample, c = idx.shape[0], idx.shape[1], feat.shape[1]
+    xyz = torch.cat([xyz, torch.zeros([1, 3]).to(xyz.device)], dim=0)
+    feat = torch.cat([feat, torch.zeros([1, c]).to(feat.device)], dim=0)
+    grouped_feat = feat[idx.view(-1).long(), :].view(m, nsample, c)  # (m, num_sample, c)
+
+    if with_xyz:
+        assert new_xyz.is_contiguous()
+        mask = torch.sign(idx + 1)
+        grouped_xyz = xyz[idx.view(-1).long(), :].view(m, nsample, 3) - new_xyz.unsqueeze(1)  # (m, num_sample, 3)
+        grouped_xyz = torch.einsum("n s c, n s -> n s c", grouped_xyz, mask)  # (m, num_sample, 3)
+        return torch.cat((grouped_xyz, grouped_feat), -1)
+    else:
+        return grouped_feat
+
 
 def queryandgroup(nsample, xyz, new_xyz, feat, idx, offset, new_offset, use_xyz=True):
     """
